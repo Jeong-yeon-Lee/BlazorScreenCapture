8d70af9eb7b6e608dedb8de1c25cadcdf30a637f
정연 이 <jylee@langcode.io>
2022-01-14 18:00:19 +0900

    hidden scroll 완성 !

---
+++ Client/wwwroot/js/hiddenScroll.js
@@ -0,0 +1,30 @@
+﻿var isScrolling;
+
+function scrollTest() {
+window.addEventListener('scroll', this.handleScroll, true);
+handleScroll = (e) => {
+    if (e.target.classList.contains("custom-scroll-hidden") === false) {
+        e.target.classList.add("custom-scroll-hidden");
+    }
+    }
+    console.log("스크로로로롤");
+
+
+    // Clear our timeout throughout the scroll
+    window.clearTimeout(isScrolling);
+
+     //Set a timeout to run after scrolling ends
+    isScrolling = setTimeout(function () {
+
+         //Run the callback
+        clearScroll();
+        console.log('Scrolling has stopped.');
+
+    }, 66);
+  
+}
+
+function clearScroll() {
+    document.getElementById("scrollBox").classList.remove("custom-scroll-hidden");
+    console.log("숨겼당!");
+}
