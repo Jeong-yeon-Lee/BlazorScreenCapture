41811ae20155c4b8690a20aed22f79e5e54757cb
정연 이 <jylee@langcode.io>
2022-01-14 11:48:30 +0900

    timer, resize,

---
+++ Client/wwwroot/js/resizer.js
@@ -0,0 +1,105 @@
+﻿////function onResizeTest() {
+////    console.log("dhdpdp");
+
+////    document.addEventListener('DOMContentLoaded', function () {
+     
+////    const createResizableTable = function (table) {
+////        const cols = table.querySelectorAll('th');
+////        [].forEach.call(cols, function (col) {
+////            // Add a resizer element to the column
+////            const resizer = document.createElement('div');
+////            resizer.classList.add('resizer');
+
+////            // Set the height
+////            resizer.style.height = `${table.offsetHeight}px`;
+
+////            col.appendChild(resizer);
+
+////            createResizableColumn(col, resizer);
+////        });
+////    };
+
+////    const createResizableColumn = function (col, resizer) {
+////        let x = 0;
+////        let w = 0;
+
+////        const mouseDownHandler = function (e) {
+////            x = e.clientX;
+
+////            const styles = window.getComputedStyle(col);
+////            w = parseInt(styles.width, 10);
+
+////            document.addEventListener('mousemove', mouseMoveHandler);
+////            document.addEventListener('mouseup', mouseUpHandler);
+
+////            resizer.classList.add('resizing');
+////        };
+
+////        const mouseMoveHandler = function (e) {
+////            const dx = e.clientX - x;
+////            col.style.width = `${w + dx}px`;
+////        };
+
+////        const mouseUpHandler = function () {
+////            resizer.classList.remove('resizing');
+////            document.removeEventListener('mousemove', mouseMoveHandler);
+////            document.removeEventListener('mouseup', mouseUpHandler);
+////        };
+
+////        resizer.addEventListener('mousedown', mouseDownHandler);
+////    };
+
+////    createResizableTable(document.getElementById('resizeMe'));
+////});
+////}
+
+
+window.langcode = {
+    scrollToBottom: function (selector) {
+        let children = $(selector).children();
+        if (children.length > 0) {
+            children.last()[0].scrollIntoView(false);
+        }
+    },
+
+    syncScroll: function (selector, targetSelector) {
+        $(selector).on('scroll', function () {
+            $(targetSelector).scrollTop($(this).scrollTop());
+        });
+        $(targetSelector).scrollTop($(this).scrollTop());
+    },
+
+    prismHighlightAll: function () {
+        window.Prism.highlightAll();
+        $('.code.token').click(function () {
+            let elem = $(this);
+            console.log(elem);
+            if (elem.hasClass('code')) {
+                DotNet.invokeMethodAsync(
+                    'TensorflowInsight.Client',
+                    'CodeTokenClicked',
+                    elem.attr('data-identifier'),
+                    parseInt(elem.attr('data-line'))
+                );
+            }
+        });
+    },
+
+    markdownToHtml: function (markdown) {
+        return marked(markdown);
+    },
+
+    getDomRect: function (id) {
+        console.log("dd");
+        let rect = document.getElementById(id).getBoundingClientRect();
+        return rect;
+    },
+
+    downloadFromUrl: function (id, url, fileName) {
+        const anchorElement = document.getElementById(id);
+        anchorElement.href = url;
+        anchorElement.target = '_blank';
+        anchorElement.download = fileName ?? '';
+        anchorElement.click();
+    }
+};
---
+++ Client/wwwroot/js/timer.js
@@ -0,0 +1,49 @@
+﻿var Timer = {};
+
+Timer.nowTime;
+Timer.timerTimeTemp;
+Timer.timerTime;
+
+Timer.InitializeTimer = function() {  
+    nowTime = new Date();//관리자 or MC의 의견 만들기 완료한 시간 
+    var nowCopy = new Date(); //== nowTime
+
+    timerTimeTemp = nowCopy.setMinutes(nowCopy.getMinutes() + 3);// 3은 설정된 타이머 Minutes를 의미 
+    timerTime = new Date(timerTimeTemp);
+    console.log(nowTime);
+    console.log(timerTime);
+
+
+    Timer.startTimerTest();
+
+}
+
+
+
+Timer.startTimerTest = function() {
+    let current = new Date();//(매 초마다 변동) 현재시간 
+    let diff = timerTime - current;
+
+    if (diff < 0) {
+        document.getElementById('timer').style.color = "red";
+        return;
+    }
+
+    let displayTime = new Date(diff);
+    //console.log(displayTime);
+    let displayMunutes = displayTime.getMinutes().toLocaleString();//UI로 보여질 Minutes
+    let displaySeconds = displayTime.getSeconds().toLocaleString();//UI로 보여질 Seconds
+    if (displayMunutes < 10) {
+        displayMunutes = "0" + displayMunutes;
+    }
+    if (displaySeconds < 10) {
+        displaySeconds = "0" + displaySeconds;
+    }
+    console.log(displayMunutes);
+    console.log(displaySeconds);
+    document.getElementById('timer').innerHTML = displayMunutes + ":" + displaySeconds;
+
+    setTimeout(Timer.startTimerTest, 1000);
+}
+
+
